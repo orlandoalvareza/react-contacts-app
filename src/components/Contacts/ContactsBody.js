1c2a528f69d064a53ad3b3b1ba2cd7870e0e6d38
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-28 23:07:34 -0400

    implemented a search functional input

--- src/components/Contacts/ContactsBody.js
+++ src/components/Contacts/ContactsBody.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+
 import ContactsList from './ContactsList';
 import image from '../../images/ben-sweet-2LowviVHZ-E-unsplash.jpg';
 import classes from './ContactsBody.module.css';
 
 const ContactsBody = ({ contacts }) => {
+  const [search, setSearch] = useState(contacts);
+  
+  const searchHandler = (event) => {
+    const searchedName = event.target.value;
+    const filteredContacts = contacts.filter(contacts => contacts.name.includes(searchedName));
+
+    setSearch(filteredContacts);
+  }
+
   return (
     <div className={classes["contacts-container"]}>
       <div className={classes["contacts-section"]}>
         <div className={classes["contact-header"]}>
           <h2>Contacts</h2>
           <div className={classes["contact-header__actions"]}>
-            <input type='search' id='search'/>
+            <input type='search' id='search' onChange={searchHandler} placeholder='search contact'/>
             <button type='button'>New Contact</button>
           </div>
         </div>
@@ -20,7 +31,7 @@ const ContactsBody = ({ contacts }) => {
             <h3>My Card</h3>
           </div>
         </div>
-        <ContactsList contacts={contacts}/>
+        <ContactsList contacts={search}/>
       </div>
     </div>
   )

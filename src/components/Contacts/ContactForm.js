568517976cd7001be5a8ba23bae5d84d75b3a728
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-29 00:06:26 -0400

    added a basic contact form component

--- src/components/Contacts/ContactForm.js
+++ src/components/Contacts/ContactForm.js
@@ -1,7 +1,25 @@
+import classes from './ContactForm.module.css';
+
 const ContactForm = () => {
   return (
     <form>
-
+      <div className={classes["basic-information"]}>
+        <input type='text' id="firstname" name="firstname" placeholder="First name" required/>
+        <input type="text" id="lastname" name="lastname" placeholder="Last name" />
+        <input type="text" id="company" name="company" placeholder="Company" />
+      </div>
+      <div className={classes["contact-information"]}>
+        <input type="tel" id="phone" name="phone" placeholder="Phone number" required />
+        <input type="email" id="email" name="email" placeholder="Email" />
+      </div>
+      <div className={classes["extra-information"]}>
+        <input type="text" id="address" name="address" placeholder="Address" />
+        <input type="date" id="date" name='date' placeholder='Birthday' />
+      </div>
+      <div className={classes.actions}>
+        <button type='button'>Cancel</button>
+        <button type='submit'>Done</button>
+      </div>
     </form>
   )
 }

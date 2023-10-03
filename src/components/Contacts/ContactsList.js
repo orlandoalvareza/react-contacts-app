a8a5eee63cd88ebd9b838b468926d0b58d4548e6
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-10-03 01:08:54 -0400

    sorted contacts list by name

--- src/components/Contacts/ContactsList.js
+++ src/components/Contacts/ContactsList.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import classes from './ContactsList.module.css';
 
 const ContactsList = ({ contacts }) => {
+  const sortedContacts = contacts.sort((firstItem, secondItem) => firstItem.name.localeCompare(secondItem.name));
+
   return (
     <ul className={classes["contact-list"]}>
-      {contacts.map(contact => 
+      {sortedContacts.map(contact => 
         <li key={contact.id}>
           <Link to={contact.id}>
             {contact.name}

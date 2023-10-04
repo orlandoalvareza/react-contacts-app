a934fe556786ff4175e3ed495a54f1fb3220e813
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-10-04 01:03:06 -0400

    added a delete contact verification

--- src/components/Contacts/ContactItem.js
+++ src/components/Contacts/ContactItem.js
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import { Link, useSubmit } from 'react-router-dom';
 
+import Modal from '../UI/Modal';
 import classes from './ContactItem.module.css';
 
 const ContactItem = ({ contact }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
   const submit = useSubmit();
 
+  const cancelDeleteHandler = () => {
+    setIsDeleting(false);
+  }
+
   const startDeleteHandler = () => {
+    setIsDeleting(true);
+  }
+
+  const deleteHandler = () => {
     submit(null, { method: 'delete' });
   }
+
+  const deleteModal = (
+    <Modal>
+      <div className={classes['delete-information']}>
+        <h2>Are you sure?</h2>
+        <p>This action is irreversible</p>
+      </div>
+      <div className={classes['delete-actions']}>
+        <button onClick={cancelDeleteHandler}>Cancel</button>
+        <button onClick={deleteHandler}>Delete</button>
+      </div>
+    </Modal>
+  )
 
   return (
     <div className={classes['contact-container']}>
@@ -17,6 +41,7 @@ const ContactItem = ({ contact }) => {
         <button type='button' onClick={startDeleteHandler}>Delete</button>
         <Link to='edit'>Edit</Link>
       </div>
+      {isDeleting && deleteModal}
     </div>  
   )
 }

3cce7fa195b4a079b75c1e0005d321ccefe5bf66
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-10-17 01:58:45 -0400

    installed framer motion package and implemented in home page

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom';
+import { motion, useScroll, useTransform } from 'framer-motion';
+
 import classes from './Hero.module.css';
 
 const Hero = () => {
+  const { scrollY } = useScroll();
+
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.1]);
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 100, 150, 220]);
+  const opacityHero = useTransform(
+    scrollY,
+    [0, 400, 450, 500, 600], 
+    [1, 0.8, 0.8, 0.6, 0]
+  );
+
   return (
-    <div className={classes["hero-container"]}>
-      <div className={classes["hero-content"]}>
+    <motion.div style={{ opacity: opacityHero }} className={classes["hero-container"]}>
+      <motion.div style={{ scale: scaleText, y: yText }} className={classes["hero-content"]}>
         <h1>Welcome to TouchBase</h1>
         <h3>Where Contacts Become Connections</h3>
         <Link to='/contacts'>Get Started</Link>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
